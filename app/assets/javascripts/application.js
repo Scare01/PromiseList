73750204d8ef7af60e6fd4fbb86d86637ab888bc
Alexey Slesarev <lexsl84@gmail.com>
2017-05-16 10:52:47 +0000

    add date today

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,13 +2,23 @@ document.addEventListener("DOMContentLoaded",
   function() {
   
     var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     var days_in_months;
+    var date_today = Date().getDate();
+    var day_today = weekday[Date().getDay()];
+    var year_today = Date().getFullYear();
+    var month_today = Date().getMonth();
     
+    var full_date_today = "Today is " +  + day_today 
     for (var i=0; i < month.length; i++){
-      if(i === new Date().getMonth()){
+      if(i === Date().getMonth()){
         document.getElementById("current_month").innerHTML = month[i];
       }
     }
+    
+        
+    
+    document.getElementById("date_today").innerHTML = new Date();
     
     switch (new Date().getMonth()) {
       case 0, 2, 4, 6, 7, 9, 11:
