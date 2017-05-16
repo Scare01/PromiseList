1ef1b0627bcc58c6ead5d3dcf1d854efdb1b7093
Alexey Slesarev <lexsl84@gmail.com>
2017-05-16 18:52:14 +0700

    bla

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded",
   function() {
-  
+
     var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     var days_in_months;
-    var date_today = Date().getDate();
-    var day_today = weekday[Date().getDay()];
-    var year_today = Date().getFullYear();
-    var month_today = Date().getMonth();
-    
-    var full_date_today = "Today is " +  + day_today 
+    // var date_today = Date().getDate();
+    // var day_today = weekday[Date().getDay()];
+    // var year_today = Date().getFullYear();
+    // var month_today = Date().getMonth();
+    //
+    // var full_date_today = "Today is " +  + day_today ;
     for (var i=0; i < month.length; i++){
-      if(i === Date().getMonth()){
+      if(i === new Date().getMonth()){
         document.getElementById("current_month").innerHTML = month[i];
       }
     }
-    
-        
-    
-    document.getElementById("date_today").innerHTML = new Date();
-    
+
+
+
+    // document.getElementById("date_today").innerHTML = new Date();
+
     switch (new Date().getMonth()) {
       case 0, 2, 4, 6, 7, 9, 11:
         days_in_months = 31;
@@ -31,8 +31,8 @@ document.addEventListener("DOMContentLoaded",
         days_in_months = 30;
         break;
     }
-    
-    
+
+
   // Change month to left side
     document.getElementById("left_btn").addEventListener("click",
       function(){
@@ -40,41 +40,33 @@ document.addEventListener("DOMContentLoaded",
         for (var i = 0; i < month.length; i++) {
           var month_on_page = document.getElementById("current_month").innerHTML;
         if (month[i] === month_on_page) {
-          var new_month = month[i-1]; 
+          var new_month = month[i-1];
         }
       }
       document.getElementById("current_month").innerHTML = new_month;
       month_on_page = new_month;
-      
+
       if (month_on_page === "January"){
         document.getElementById("left_btn").disabled = true;
       }
-      
-      
-    });  
-  // Change month to rigth side  
+
+
+    });
+  // Change month to rigth side
     document.getElementById("right_btn").addEventListener("click",
     function(){
       document.getElementById("left_btn").disabled = false;
       for (var i = 0; i < month.length; i++) {
         var month_on_page = document.getElementById("current_month").innerHTML;
         if (month[i] === month_on_page) {
-          var new_month = month[i+1]; 
+          var new_month = month[i+1];
         }
       }
       document.getElementById("current_month").innerHTML = new_month;
       month_on_page = new_month;
-      
+
       if (month_on_page === "December"){
         document.getElementById("right_btn").disabled = true;
       }
-    });  
+    });
   })
-  
-  
-  
- 
-
-
-
-
