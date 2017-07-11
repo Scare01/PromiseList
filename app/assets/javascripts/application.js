e80b437ad379fdf7e32e31c6e4cb4f80a17ea4f0
Alexey Slesarev <lexsl84@gmail.com>
2017-07-11 10:31:38 +0000

    add table with ruby

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,123 +5,126 @@
 //= require_tree 
 
 
-var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var day_today = weekday[new Date().getDay()];
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+// var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// var day_today = weekday[new Date().getDay()];
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
   
   // Reload page for home button
-    document.getElementById("go_home").addEventListener("click", function(){
-     document.location.reload();
-    });
+    // document.getElementById("go_home").addEventListener("click", function(){
+    // document.location.reload();
+    // });
   
   // document.observe('dom:loaded', function() {
 
 // Add current month on page
-  for (var i=0; i < month.length; i++){
-    if(i === new Date().getMonth()){
-      document.getElementById("current_month").innerHTML = month[i];
-    }
-  }
+  // for (var i=0; i < month.length; i++){
+  //   if(i === new Date().getMonth()){
+  //     document.getElementById("current_month").innerHTML = month[i];
+  //   }
+  // }
  
 
 // add full date to page
-  var full_date_today = "Today is " + day_today + ", " + new Date().getDate() + " " + month[new Date().getMonth()] + " " + new Date().getFullYear() ;
-  document.getElementById("date_today").innerHTML = full_date_today;
+  // var full_date_today = "Today is " + day_today + ", " + new Date().getDate() + " " + month[new Date().getMonth()] + " " + new Date().getFullYear() ;
+  // document.getElementById("date_today").innerHTML = full_date_today;
 
 // Change month to left side
-    document.getElementById("left_btn").addEventListener("click",
-      function(){
-        document.getElementById("right_btn").disabled = false;
-        for (var i = 0; i < month.length; i++) {
-          var month_on_page = document.getElementById("current_month").innerHTML;
-          if (month[i] === month_on_page) {
-            var new_month = month[i-1];
-          }
-        }
-      document.getElementById("current_month").innerHTML = new_month;
-      month_on_page = new_month;
+    // document.getElementById("left_btn").addEventListener("click",
+    //   function(){
+    //     document.getElementById("right_btn").disabled = false;
+    //     for (var i = 0; i < month.length; i++) {
+    //       var month_on_page = document.getElementById("current_month").innerHTML;
+    //       if (month[i] === month_on_page) {
+    //         var new_month = month[i-1];
+    //       }
+    //     }
+    //   document.getElementById("current_month").innerHTML = new_month;
+    //   month_on_page = new_month;
 
-      if (month_on_page === "January"){
-        document.getElementById("left_btn").disabled = true;
-      }
-    });
+    //   if (month_on_page === "January"){
+    //     document.getElementById("left_btn").disabled = true;
+    //   }
+    // });
 // Change month to rigth side
-    document.getElementById("right_btn").addEventListener("click",
-    function(){
-      document.getElementById("left_btn").disabled = false;
-      for (var i = 0; i < month.length; i++) {
-        var month_on_page = document.getElementById("current_month").innerHTML;
-        if (month[i] === month_on_page) {
-          var new_month = month[i+1];
-        }
-      }
-      document.getElementById("current_month").innerHTML = new_month;
-      month_on_page = new_month;
+    // document.getElementById("right_btn").addEventListener("click",
+    // function(){
+    //   document.getElementById("left_btn").disabled = false;
+    //   for (var i = 0; i < month.length; i++) {
+    //     var month_on_page = document.getElementById("current_month").innerHTML;
+    //     if (month[i] === month_on_page) {
+    //       var new_month = month[i+1];
+    //     }
+    //   }
+    //   document.getElementById("current_month").innerHTML = new_month;
+    //   month_on_page = new_month;
 
-      if (month_on_page === "December"){
-        document.getElementById("right_btn").disabled = true;
-      }
-    });
+    //   if (month_on_page === "December"){
+    //     document.getElementById("right_btn").disabled = true;
+    //   }
+    // });
 
     // Add name of table
-    var table = document.getElementById("prom-table");
-    var header = table.createTHead();
-    var row = header.insertRow(0);
-    var cell = row.insertCell(0);
-    cell.innerHTML = "Promises\\Day";
+    // var table = document.getElementById("prom-table");
+    // var header = table.createTHead();
+    // var row = header.insertRow(0);
+    // var cell = row.insertCell(0);
+    // cell.innerHTML = "Promises\\Day";
     
     
     //add days of month 
-    for (var i = 0; i < month.length; i++ ){
-      var month_on_page = document.getElementById("current_month").innerHTML;
+    // for (var i = 0; i < month.length; i++ ){
+    //   var month_on_page = document.getElementById("current_month").innerHTML;
       
-          if (month[i] === month_on_page){
-            var days = days_in_month[i];
-            for (var j =1; j <=days; j++){
-              var cell = row.insertCell(j);
-              cell.innerHTML = j;
-            }
-          }
-    }  
+    //       if (month[i] === month_on_page){
+    //         var days = days_in_month[i];
+    //         for (var j =1; j <=days; j++){
+    //           var cell = row.insertCell(j);
+    //           cell.innerHTML = j;
+    //         }
+    //       }
+    // }  
     
     
-    document.getElementById("current_month").addEventListener("DOMSubtreeModified",function(){
-      document.getElementById("prom-table").deleteTHead();
+    // document.getElementById("current_month").addEventListener("DOMSubtreeModified",function(){
+    //   document.getElementById("prom-table").deleteTHead();
       
        // Add name of table
-      var table = document.getElementById("prom-table");
-      var header = table.createTHead();
-      var row = header.insertRow(0);
-      var cell = row.insertCell(0);
-      cell.innerHTML = "Promises\\Day";
+      // var table = document.getElementById("prom-table");
+      // var header = table.createTHead();
+      // var row = header.insertRow(0);
+      // var cell = row.insertCell(0);
+      // cell.innerHTML = "Promises\\Day";
     
     
-        //add days of month 
-      for (var i = 0; i < month.length; i++ ){
-        var month_on_page = document.getElementById("current_month").innerHTML;
+      //   //add days of month 
+      // for (var i = 0; i < month.length; i++ ){
+      //   var month_on_page = document.getElementById("current_month").innerHTML;
       
-          if (month[i] === month_on_page){
-            var days = days_in_month[i];
-            for (var j =1; j <=days; j++){
-              cell = row.insertCell(j);
-              cell.innerHTML = j;
-            }
-          }
-      } 
+      //     if (month[i] === month_on_page){
+      //       var days = days_in_month[i];
+      //       for (var j =1; j <=days; j++){
+      //         cell = row.insertCell(j);
+      //         cell.innerHTML = j;
+      //       }
+      //     }
+      // } 
       
   // add days in each new row
     
     
     
-    document.getElementById("prom-table").addEventListener("DOMAttrModified", 
-      function(){
-        alert("added elemetn");
-      });
+    // document.getElementById("prom-table").addEventListener("DOMAttrModified", 
+    //   function(){
+    //     alert("added elemetn");
+    //   });
       
     
     
@@ -170,13 +173,13 @@ document.addEventListener("DOMContentLoaded", function() {
       
       
       
-    });
+    // });
     
   
  
     // DOMNodeInserted
     
 
-  })
+  // })
 
 
